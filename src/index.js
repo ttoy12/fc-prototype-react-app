b2e1101d39207227813f66be35435c1271447e08
Thomas Toy <thomastoy1213@gmail.com>
2023-03-01 18:30:56 -0800

    fixed react 17 error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-  
-ReactDOM.render(
-<React.StrictMode>
-    <App />
-</React.StrictMode>,
-document.getElementById('root')
+//import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+    //<StrictMode>
+        <App />
+    //</StrictMode>
 );
+
+// ReactDOM.render(  // React 17
+// <React.StrictMode>
+//     <App />
+// </React.StrictMode>,
+// document.getElementById('root')
+// );
